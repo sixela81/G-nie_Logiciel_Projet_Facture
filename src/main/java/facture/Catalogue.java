6e5eab1c6a01bf68f25681fb6975c7c144d2469b
Rémi Bastide <Remi.Bastide@irit.fr>
2017-11-29 16:04:03 +0100

    First commit

---
+++ src/main/java/facture/Catalogue.java
@@ -0,0 +1,15 @@
+package facture;
+
+public class Catalogue {
+    
+    public void addArticle(Article article) {
+         // TODO: Implémenter cette méthode
+        throw new UnsupportedOperationException("Pas encore implémenté");
+    }
+    
+    public Article findByCode(String code) {
+         // TODO: Implémenter cette méthode
+        throw new UnsupportedOperationException("Pas encore implémenté");
+    }
+    
+}
---
+++ src/main/java/facture/Facture.java
@@ -0,0 +1,60 @@
+package facture;
+
+import java.io.PrintStream;
+import java.util.Date;
+
+public class Facture {
+
+    public Facture(Client destinataire, Date emission, int numero) {
+         // TODO: Implémenter cette méthode
+        throw new UnsupportedOperationException("Pas encore implémenté");
+    }
+
+    /**
+     * Get the value of numero
+     *
+     * @return the value of numero
+     */
+    public int getNumero() {
+         // TODO: Implémenter cette méthode
+        throw new UnsupportedOperationException("Pas encore implémenté");
+    }
+
+    /**
+     * Get the value of emission
+     *
+     * @return the value of emission
+     */
+    public Date getEmission() {
+         // TODO: Implémenter cette méthode
+        throw new UnsupportedOperationException("Pas encore implémenté");
+    }
+
+    /**
+     * Get the value of destinataire
+     *
+     * @return the value of destinataire
+     */
+    public Client getDestinataire() {
+         // TODO: Implémenter cette méthode
+        throw new UnsupportedOperationException("Pas encore implémenté");
+    }
+
+
+    
+    public void ajouteLigne(Article a, int nombre, float remise) {
+         // TODO: Implémenter cette méthode
+        throw new UnsupportedOperationException("Pas encore implémenté");
+   }
+    
+   public float montantTTC(float tauxTVA) {
+         // TODO: Implémenter cette méthode
+        throw new UnsupportedOperationException("Pas encore implémenté");
+   }
+   
+   public void print(PrintStream out, float tva) {
+         // TODO: Implémenter cette méthode
+        throw new UnsupportedOperationException("Pas encore implémenté");
+   }
+   
+}
